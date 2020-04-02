e0051d5a41c2e96ae124c0dcef20a1dfbd9fb456
Basanta78 <basmhzn78@gmail.com>
2020-04-02 22:19:27 +0545

    Add youtube download util

---
+++ musicrs/util/youtube.py
@@ -0,0 +1,35 @@
+""" YoutubeDL utility """
+
+from __future__ import unicode_literals
+import youtube_dl
+
+
+def download_from_youtube(video_links:list, output_format:str, download_path:str)
+    """
+    Download video/audio from youtube link
+    :param video_links: list of youtube links
+    :param output_format: output format
+    :param download_path: download path
+    """
+    params = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': output_format,
+            'preferredquality': '192'
+
+        }],
+        'postprocessor_args': [
+            '-ar', '16000'
+
+        ],
+        'prefer_ffmpeg': False,
+        'keepvideo': False,
+        'outtmpl': '{}%(title)s.%(ext)s'.format(download_path),
+        'noplaylist': True,
+        'extractaudio': True
+
+
+    }
+    with youtube_dl.YoutubeDL(params) as ydl:
+        ydl.download(video_links)
