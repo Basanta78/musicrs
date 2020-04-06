1b1894172aeaa02ce365898c58d7d3cb1b2f9e34
Basanta78 <basmhzn78@gmail.com>
2020-04-06 23:33:35 +0545

    Add requirements for recommendation engine

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ import setuptools
 import musicrs
 
 # Requirements
-requirements = ["pyodbc>=4.0.27,<5", "flatten_json==0.1.7", "boto3==1.12.6", "youtube-dl==2020.3.24"]
+requirements = ["pyodbc>=4.0.27,<5", "flatten_json==0.1.7", "boto3==1.12.6",
+                "youtube-dl==2020.3.24", "librosa==0.6.0", "matplotlib==3.2.1",
+                "mxnet-cu90==1.6.0", "mxnet==1.6.0", "lru-dict==1.1.6", "scipy==1.4.1",
+                "scikit-learn==0.22.2.post1"]
 
 # Development Requirements
 requirements_dev = ["pytest==4.*", "mock==3.0.5", "black==19.10b0", "moto==1.3.14"]
