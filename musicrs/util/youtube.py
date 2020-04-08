684c4a6291f02b1a1f0f0b569129f5bbbf174dcb
ShishirBhandari <shishirbhandari54@gmail.com>
2020-04-08 12:03:48 +0545

    Use black formatter

--- musicrs/util/youtube.py
+++ musicrs/util/youtube.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 import youtube_dl
 
 
-def download_from_youtube(video_links:list, output_format:str, download_path:str)
+def download_from_youtube(video_links: list, output_format: str, download_path: str):
     """
     Download video/audio from youtube link
     :param video_links: list of youtube links
@@ -12,24 +12,20 @@ def download_from_youtube(video_links:list, output_format:str, download_path:str
     :param download_path: download path
     """
     params = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': output_format,
-            'preferredquality': '192'
-
-        }],
-        'postprocessor_args': [
-            '-ar', '16000'
-
+        "format": "bestaudio/best",
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": output_format,
+                "preferredquality": "192",
+            }
         ],
-        'prefer_ffmpeg': False,
-        'keepvideo': False,
-        'outtmpl': '{}%(title)s.%(ext)s'.format(download_path),
-        'noplaylist': True,
-        'extractaudio': True
-
-
+        "postprocessor_args": ["-ar", "16000"],
+        "prefer_ffmpeg": False,
+        "keepvideo": False,
+        "outtmpl": "{}%(title)s.%(ext)s".format(download_path),
+        "noplaylist": True,
+        "extractaudio": True,
     }
     with youtube_dl.YoutubeDL(params) as ydl:
         ydl.download(video_links)
