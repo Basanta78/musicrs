4ecdcf5a162eabfaa00cf155f23a98bd5b42f9f8
ShishirBhandari <shishirbhandari54@gmail.com>
2020-04-09 09:11:02 +0545

    slack: Parse retrieved slack messages and return useful information

--- musicrs/util/slack_api.py
+++ musicrs/util/slack_api.py
@@ -15,8 +15,30 @@ slackClient = slack.WebClient(token=SLACK_TOKEN)
 def retrieve_slack_messages(channel: str):
     """
     Retrieve youtube links from slack channel
+    :param channel: Slack channel id
+    :type channel: string
+    :return slack channel messages
+    :rtype list
     """
     response = slackClient.conversations_history(channel=channel)
-    messages = response["messages"]
-    for m in messages:
-        pass
+    retrieved_messages = []
+
+    for message in response["messages"]:
+        retrieved_message = {}
+
+        if message["type"] == "message":
+            retrieved_message["user"] = message["user"]
+            retrieved_message["timestamp"] = message["ts"]
+
+            if message.get("attachments"):
+                attached_song = message["attachments"][0]
+                song = {
+                    "song_title": attached_song["title"],
+                    "song_author": attached_song["author_name"],
+                    "song_url": attached_song["original_url"],
+                }
+
+                retrieved_message.update(song)
+                retrieved_messages.append(retrieved_message)
+
+    return retrieved_messages
