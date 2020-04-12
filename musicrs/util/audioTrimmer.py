3b6c2f582275c3f28093732f55f203ce4c957f09
Basanta78 <basmhzn78@gmail.com>
2020-04-12 12:16:10 +0545

    Reformat code with black

--- musicrs/util/audioTrimmer.py
+++ musicrs/util/audioTrimmer.py
@@ -1,18 +1,22 @@
 from pydub import AudioSegment
 import sys
-sys.path.append('/usr/bin/ffmpeg')
 
-def audioTrimmer(files_path,file_name,fileType,startMin,startSec,endMin,endSec):
-  MIN = 60
-  MILI_SECOND = 1000
+sys.path.append("/usr/bin/ffmpeg")
 
-  # Time to miliseconds
-  startTime = startMin*MIN*MILI_SECOND+startSec*MILI_SECOND
-  endTime = endMin*MIN*MILI_SECOND+endSec*MILI_SECOND
 
-  # Opening file and extracting segment
-  segment = AudioSegment.from_file( files_path+'/'+file_name, fileType )
-  extract = segment[startTime:endTime]
+def audioTrimmer(files_path, file_name, fileType, startMin, startSec, endMin, endSec):
+    MIN = 60
+    MILI_SECOND = 1000
 
-  # Saving
-  extract.export( files_path+'/'+file_name+'-extract.'+fileType, format=fileType)
+    # Time to miliseconds
+    startTime = startMin * MIN * MILI_SECOND + startSec * MILI_SECOND
+    endTime = endMin * MIN * MILI_SECOND + endSec * MILI_SECOND
+
+    # Opening file and extracting segment
+    segment = AudioSegment.from_file(files_path + "/" + file_name, fileType)
+    extract = segment[startTime:endTime]
+
+    # Saving
+    extract.export(
+        files_path + "/" + file_name + "-extract." + fileType, format=fileType
+    )
