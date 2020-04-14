a8848bbefa5d230645d1fa8d35167483cdf75c18
Basanta78 <basmhzn78@gmail.com>
2020-04-14 18:40:14 +0545

    Add database settings

--- musicrs/settings.py
+++ musicrs/settings.py
@@ -10,3 +10,11 @@ dotenv.load_dotenv()
 SLACK_API_TOKEN = os.getenv("SLACK_API_TOKEN")
 SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
 SLACK_CHANNEL_ID = os.getenv("SLACK_CHANNEL_ID")
+DATABASE_CONN = {
+    "drivername": os.getenv("DRIVERNAME", "postgresql"),
+    "host": os.getenv("HOST", "localhost"),
+    "port": os.getenv("PORT", "5432"),
+    "username": os.getenv("DB_USER", "postgres"),
+    "password": os.getenv("DB_PASSWORD", "password"),
+    "database": os.getenv("DATABASE", "default"),
+}
