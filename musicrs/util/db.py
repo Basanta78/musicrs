bed5217463c6d70a436a56d19a42781a92895049
Basanta78 <basmhzn78@gmail.com>
2020-04-15 18:22:58 +0545

    Add method to connect to db

--- musicrs/util/db.py
+++ musicrs/util/db.py
@@ -1,56 +1,41 @@
 """ Database utility module. """
 
 import pyodbc
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+from sqlalchemy.orm import sessionmaker
 
-from musicrs.util.object import merge
-
-from musicrs.api.logging import get_logger
-
-# Database connection types
-PG: str = "pg"
-MSSQL: str = "mssql"
-MYSQL: str = "mysql"
-
-# Drivers
-DRIVER_PG = "{PostgreSQL Unicode}"
-DRIVER_MSSQL = "{ODBC Driver 17 for SQL Server}"
-
-# Defaults
-DEFAULTS = {}
-DEFAULTS[PG] = {"driver": DRIVER_PG, "port": "5432"}
-DEFAULTS[MSSQL] = {"driver": DRIVER_MSSQL, "port": "1433"}
-DEFAULTS[MYSQL] = {"driver": "{MySQL ODBC 8.0 Driver}", "port": "3306"}
-
-# Connection strings
-CONN_STR: str = ";".join(
-    [
-        "DRIVER={driver}",
-        "SERVER={host}",
-        "PORT={port}",
-        "DATABASE={database}",
-        "UID={username}",
-        "PWD={password}",
-    ]
-)
-
-logger = get_logger("db")
+from musicrs.model.base import Base
+from musicrs.settings import DATABASE_CONN
 
 
-def build_connstr(**args) -> str:
-    """ Build commection string from the received parameters. """
-    # Database client type to connect.
-    client = args.get("client")
 
-    if not client:
-        raise RuntimeError("Database `client` must be provided to connect to.")
+def create_db_engine():
+    """ Create database connection engine"""
+    database_url = URL(**DATABASE_CONN)
+    db_engine = create_engine(database_url)
+    return db_engine
 
-    defaults = DEFAULTS.get(client)
 
-    if not defaults:
-        raise RuntimeError('Unsupported database client type "{}".'.format(client))
+def create_db_session():
+    """ Create database session """
+    db_engine = create_db_engine()
+    session = sessionmaker(bind=db_engine)
+    return session
 
-    params = merge(defaults, args)
 
-    connstr = CONN_STR.format(**params)
+def create_tables(engine):
+    """ Create tables from metadata """
+    Base.metadata.create_all(engine)
 
-    return connstr
+
+def drop_tables(engine):
+    """ Drop all the tables """
+    Base.metadata.drop_all(engine)
+
+
+def reset_db():
+    """ Reset database"""
+    db_engine = create_db_engine()
+    drop_tables(engine=db_engine)
+    create_tables(engine=db_engine)
