e343659847bea5f63ed69697e110eb514f11378e
Basanta78 <basmhzn78@gmail.com>
2020-04-20 21:05:33 +0545

    Add inference cli command

--- musicrs/cli.py
+++ musicrs/cli.py
@@ -1,6 +1,7 @@
 """ Musicrs click command"""
 import click
 from musicrs.util.db import reset_db
+from musicrs.recommendation_engine.inference import load_inference
 
 
 @click.group(invoke_without_command=False)
@@ -11,3 +12,8 @@ def main():
 @main.command()
 def reset():
     reset_db()
+
+
+@main.command()
+def inference():
+    load_inference()
