e38af7c395813f549bf3d3e54a716880e50bd6cd
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-04-21 19:03:19 +0545

    Split function to process files in folder and individual files

--- musicrs/util/youtubeIdToLink.py
+++ musicrs/util/youtubeIdToLink.py
@@ -1,14 +1,16 @@
 import os
 
-def youtubeIdToLink(basePath):
+def youtube_id_to_link(basePath):
+  fileLists = os.listdir(basePath)
+  for fileName in fileLists:
+    read_file_convert(basePath,fileName)
 
+def read_file_convert(basePath,fileName):
   baseUrl = 'https://www.youtube.com/watch?v='
 
-  entries = os.listdir(basePath)
-  for entry in entries:
-    f=open(os.path.join(basePath,entry), "r")
-    if f.mode == 'r':
-      contents =f.readlines()
-      for youtubeId in contents: 
-        if not '# new artist:' in youtubeId: 
-          print(baseUrl+youtubeId)
+  f=open(os.path.join(basePath,fileName), "r")
+  if f.mode == 'r':
+    contents =f.readlines()
+    for youtubeId in contents: 
+      if not '#' in youtubeId: 
+        print(baseUrl+youtubeId)
