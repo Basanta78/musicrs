2cda2e20df93bf18db1da613c25db2fc97100e96
Basanta78 <basmhzn78@gmail.com>
2020-04-26 18:48:52 +0545

    Add similarity check funtionality

---
+++ musicrs/recommendation_engine/similarity.py
@@ -0,0 +1,44 @@
+import faiss
+
+
+def index_database_vector(dimension, database_vectors):
+    """
+    Index the database vector(i.e our song embeddings) using faiss
+    :param dimension: dimension of the database vector
+    :param database_vectors: database vector/song embeddings
+    :return: index
+    """
+
+    faiss.normalize_L2(database_vectors)  # Normalize the vectors
+
+    index = faiss.IndexFlatIP(
+        dimension
+    )  # Initialize index for the vector with dimension d
+    index.add(database_vectors)  # Add database vector for indexing
+
+    return index
+
+
+def search(index, user_vector, k=1):
+    """
+    Search most similar vector from database_vector given query_vector
+
+    :param index: index
+    :param user_vector: query vectors of a user
+    :param k: number of nearest neighbour vector for each query
+    :return: similar sorted vector result
+    """
+
+    faiss.normalize_L2(user_vector)
+    cosine, near_index = index.search(
+        user_vector, k
+    )  # Returns cosine similarity and index of the nearest neighbour vectors
+
+    result_tuple = zip(cosine.flatten(), near_index.flatten())
+
+    # Sort the result in descending order so the most similar is always the first
+    similar_vector_index_sorted = []
+    for x in result_tuple:
+        similar_vector_index_sorted.append(x)
+    # similar_vector_index_sorted.sort(key=operator.itemgetter(0), reverse=True)
+    return similar_vector_index_sorted
