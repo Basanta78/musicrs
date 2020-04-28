bcf67de1410ea6bf7b3503943e33382f5ba987a5
Basanta78 <basmhzn78@gmail.com>
2020-04-28 22:21:36 +0545

    Add user-message cli

--- musicrs/cli.py
+++ musicrs/cli.py
@@ -1,7 +1,9 @@
 """ Musicrs click command"""
 import click
+from datetime import timedelta, date
 from musicrs.util.db import reset_db
 from musicrs.recommendation_engine.inference import load_inference
+from musicrs.recommendation_engine.user_profile import load_slack_messages
 
 
 @click.group(invoke_without_command=False)
@@ -15,6 +17,15 @@ def reset():
 
 
 @main.command()
-@click.option("--file-path", required=True)
+@click.option("--file-path", "-p", required=True)
 def inference(file_path):
     load_inference(file_path)
+
+
+@main.command()
+@click.option(
+    "--start-date", "-s", required=False, default=date.today() - timedelta(days=10)
+)
+@click.option("--end-date", "-e", required=False, default=date.today())
+def user_message(start_date, end_date):
+    load_slack_messages(start_date, end_date)
