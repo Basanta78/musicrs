eb3aeff7d4101fe85b9a7a04d8e1cf7f3713a4d2
Basanta78 <basmhzn78@gmail.com>
2020-04-28 23:06:00 +0545

    Add music recommend cli

--- musicrs/cli.py
+++ musicrs/cli.py
@@ -3,6 +3,7 @@ import click
 from datetime import timedelta, date
 from musicrs.util.db import reset_db
 from musicrs.recommendation_engine.inference import load_inference
+from musicrs.recommendation_engine.recommend import recommend_music
 from musicrs.recommendation_engine.user_profile import load_slack_messages
 
 
@@ -29,3 +30,9 @@ def inference(file_path):
 @click.option("--end-date", "-e", required=False, default=date.today())
 def user_message(start_date, end_date):
     load_slack_messages(start_date, end_date)
+
+
+@main.command()
+@click.option("--user-id", "-u", required=True)
+def recommend(user_id):
+    recommend_music(user_id)
