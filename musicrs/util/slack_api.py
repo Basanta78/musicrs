b5686c6a6e15123a9ae1258d6a1e1e51d82c9910
ShishirBhandari <shishirbhandari54@gmail.com>
2020-05-11 08:34:07 +0545

    slack-bot: remove duplicate methods

--- musicrs/util/slack_api.py
+++ musicrs/util/slack_api.py
@@ -7,11 +7,6 @@ import slack
 import musicrs.settings as settings
 from musicrs.util.date_time import *
 
-from musicrs.model.base import UserProfile
-from musicrs.model.db_session import session_scope
-from musicrs.util.youtube import get_video_id, generate_url
-from musicrs.util.numpy import serialize
-from musicrs.recommendation_engine.inference import generate_inference
 
 SLACK_API_TOKEN = settings.SLACK_API_TOKEN
 SLACK_BOT_TOKEN = settings.SLACK_BOT_TOKEN
@@ -78,60 +73,19 @@ def retrieve_slack_messages(channel: str, start_date: str, end_date: str):
     return retrieved_messages
 
 
-def post_slack_message(user_id, video_id):
+def post_slack_message(user_id, video_url):
     """
     Sends video url to the user.
     param user_id: user's member id in slack workspace
     type user_id: string
-    param video_id: youtube video id
-    type video_id: string
+    param video_url: youtube video url
+    type video_url: string
     """
     try:
         dm_response = slackBotClient.conversations_open(users=user_id)
         dm_channel = dm_response["channel"]["id"]
-        video_url = generate_url(video_id=video_id)
 
         response = slackBotClient.chat_postMessage(channel=dm_channel, text=video_url)
         print("Posted {0} to the user {1}".format(video_url, user_id))
     except:
         print("Cannot post video to the user: " + str(user_id))
-
-
-def dump_slack_to_db(slack_message, serialized_np):
-    """
-    Dump slack youtube urls and user profile to db
-    :param slack_message: dict
-    :param serialized_np: str
-    """
-    video_id = get_video_id(slack_message["song_url"])
-    with session_scope() as session:
-        identity = (
-            session.query(UserProfile)
-            .filter_by(user_id=slack_message["user"], video_id=video_id)
-            .first()
-        )
-
-        if not identity:
-            user_profile = UserProfile(
-                user_id=slack_message["user"],
-                user_name=slack_message["user_real_name"],
-                user_email=slack_message["user_email"],
-                user_profile_picture=slack_message["user_profile_picture"],
-                video_id=video_id,
-                audio_encoding=serialized_np,
-            )
-            session.add(user_profile)
-
-
-def load_slack_messages(start_date, end_date):
-    """
-    Retrieve slack messages and encoding and load into db
-    :param start_date: str (YYYY-mm-dd)
-    :param end_date: str (YYYY-mm-dd)
-    """
-    slack_channel = settings.SLACK_CHANNEL_ID
-    retrieved_messages = retrieve_slack_messages(channel, start_date, end_date)
-    for slack_message in retrieved_messages:
-        inference = generate_inference(slack_message["song_url"])
-        serialized_np = serialize(inference)
-        dump_slack_to_db(slack_message, serialized_np)
